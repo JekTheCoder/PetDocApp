ea5034f7b08db572b9a2b1c8c5041ff212757421
Jeshua <hinostrozajeshua@gmail.com>
2022-10-22 22:05:58 -0500

    create: modelos proyect

---
+++ Modelos/Mascota.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Modelos
+{
+    public class Mascota
+    {
+        public int idMascota { get; set; }
+        public int idDueño { get; set; }
+        public int tipoAnimal { get; set; }
+
+        public string raza { get; set; }
+        public char sexo { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string observaciones { get; set; }
+    }
+}
