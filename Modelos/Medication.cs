c3bbffd8da710f563e66d66afb5af0bccb8d3b4b
Jeshua <hinostrozajeshua@gmail.com>
2022-10-23 18:06:08 -0500

    create: medication repository

---
+++ Modelos/Medication.cs
@@ -0,0 +1,13 @@
+﻿namespace Modelos
+{
+    public class Medication
+    {
+        public int idVisita { get; set; }
+        public string observaciones { get; set; }
+        public int idMedicamento { get; set; }
+        public string nombreMedicamento { get; set; }
+        public decimal precioMedicamento { get; set; }
+        public int tipoVisita { get; set; }
+        public string dosis { get; set; }
+    }
+}
