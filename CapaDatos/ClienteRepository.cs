92d340a0baa71759e4cf6b06e729b7ebae05480d
Jeshua <hinostrozajeshua@gmail.com>
2022-10-23 12:11:56 -0500

    create: cliente repository

--- CapaDatos/ClienteRepository.cs
+++ CapaDatos/ClienteRepository.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Modelos;
+using System.Data.SqlClient;
 
 namespace CapaDatos
 {
-    internal class ClienteRepository
+    public class ClienteRepository : BaseRepository<Cliente>, Repository<Cliente>
     {
+        public bool Create(Cliente entity)
+        {
+            return BuildCreate("create_cliente", ref entity);
+        }
+
+        public bool Delete(int id)
+        {
+            return BuildDelete("delete_cliente", id);
+        }
+
+        public Cliente[] GetAll()
+        {
+            return BuildGetAll("get_clientes");
+        }
+
+        public Cliente GetOne(int id)
+        {
+            return BuildGetOne("get_cliente", id);
+        }
+
+        public bool Update(Cliente entity)
+        {
+            return BuildUpdate("update_cliente", ref entity);
+        }
+
+        protected override void MapRequest(ref SqlCommand cmd, ref Cliente entity)
+        {
+            cmd.Parameters.AddWithValue("@id", entity.idCliente);
+            cmd.Parameters.AddWithValue("@nombreCliente", entity.nombreCliente);
+            cmd.Parameters.AddWithValue("@apellidoCliente", entity.apellidoCliente);
+            cmd.Parameters.AddWithValue("@direccion", entity.direccion);
+            cmd.Parameters.AddWithValue("@ciudad", entity.ciudad);
+            cmd.Parameters.AddWithValue("@telefono", entity.telefono);
+        }
+
+        protected override Cliente MapResponse(ref SqlDataReader reader)
+        {
+            return new Cliente
+            {
+                idCliente = reader.GetInt32(0),
+                nombreCliente = reader.GetString(1),
+                apellidoCliente = reader.GetString(2),
+                direccion = reader.GetString(3),
+                ciudad = reader.GetString(4),
+                telefono = reader.GetString(5),
+            };
+        }
     }
 }
---
+++ Modelos/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Cliente
+    {
+        public int idCliente { get; set; }
+        public string nombreCliente { get; set; }
+        public string apellidoCliente { get; set; }
+        public string direccion { get; set; }
+        public string ciudad { get; set; }
+        public string telefono { get; set; }
+    }
+}
