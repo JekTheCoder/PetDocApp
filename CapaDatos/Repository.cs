45a9aaaef1e2bb436bcc056a3247b346bd983491
Jeshua <hinostrozajeshua@gmail.com>
2022-10-22 22:08:15 -0500

    create: capaDatos proyect

---
+++ CapaDatos/Repository.cs
@@ -0,0 +1,11 @@
+﻿namespace CapaDatos
+{
+    public interface Repository<T>
+    {
+        T GetOne(int id);
+        T[] GetAll();
+        bool Create(T entity);
+        bool Update(T entity);
+        bool Delete(int id);
+    }
+}
---
+++ CapaDatos/SqlConexion.cs
@@ -0,0 +1,48 @@
+﻿using CapaDatos.Properties;
+using System;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    internal class SqlConexion
+    {
+        private static SqlConexion sqlConnection;
+
+        public static SqlConexion GetSqlConnection()
+        {
+            if (sqlConnection == null) sqlConnection = new SqlConexion();
+            return sqlConnection;
+        }
+
+        private readonly SqlConnection sql = new SqlConnection(Settings.Default.ConnectionString);
+
+        public SqlCommand BuildCommand(string command)
+        {
+            return new SqlCommand
+            {
+                CommandText = command,
+                Connection = sql
+            };
+        }
+
+        public bool SqlExecute(Action action)
+        {
+            bool success;
+
+            try
+            {
+                sql.Open();
+                action();
+                sql.Close();
+
+                success = true;
+            }
+            catch
+            {
+                success = false;
+            }
+
+            return success;
+        }
+    }
+}
