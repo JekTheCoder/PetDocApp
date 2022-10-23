404779ad58b86a492191ee0f5fb2b7ab64f83637
Jeshua <hinostrozajeshua@gmail.com>
2022-10-23 17:42:27 -0500

    create: visit repository

---
+++ CapaDatos/VisitRepository.cs
@@ -0,0 +1,55 @@
+﻿using Modelos;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class VisitRepository
+    {
+        private SqlConexion conexion = SqlConexion.GetSqlConnection();
+
+        private Visit MapRequest(ref SqlDataReader reader)
+        {
+            return new Visit
+            {
+                idVisita = reader.GetInt32(0),
+                idMascota = reader.GetInt32(1),
+                idDoctor = reader.GetInt32(2),
+                fechaVisita = reader.GetDateTime(3),
+                tipoVisita = reader.GetString(4),
+                detalles = reader.GetInt32(5),
+                precioBruto = reader.GetDecimal(6),
+                IGV = reader.GetDecimal(6),
+                preciototal = reader.GetDecimal(7)
+            };
+        }
+
+        public Visit[] GetAll()
+        {
+            var cmd = conexion.BuildCommand("showVisit");
+            var list = new List<Visit>();
+
+            conexion.SqlExecute(() =>
+            {
+                var reader = cmd.ExecuteReader();
+                while (reader.Read()) list.Add(MapRequest(ref reader));
+            });
+
+            return list.ToArray();
+        }
+
+        public Visit GetOne(int id)
+        {
+            var cmd = conexion.BuildCommand("showVisitbyID");
+            Visit visit = null;
+
+            conexion.SqlExecute(() =>
+            {
+                var reader = cmd.ExecuteReader();
+                if (reader.Read()) visit = MapRequest(ref reader);
+            });
+
+            return visit;
+        }
+    }
+}
---
+++ Modelos/Visit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modelos
+{
+    public class Visit
+    {
+        public int idVisita { get; set; }
+        public int idMascota { get; set; }
+        public int idDoctor { get; set; }
+        public DateTime fechaVisita { get; set; }
+        public string tipoVisita { get; set; }
+        public int detalles { get; set; }
+        public decimal precioBruto { get; set; }
+        public decimal IGV { get; set; }
+        public decimal preciototal { get; set; }
+    }
+}
