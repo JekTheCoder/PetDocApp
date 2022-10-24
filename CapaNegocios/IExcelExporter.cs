b805c8b3584ca342aeb4bb93e1a733f5c5e3afb4
Jeshua <hinostrozajeshua@gmail.com>
2022-10-24 17:27:21 -0500

    create: base-excel-exporter

---
+++ CapaNegocios/IExcelExporter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CapaNegocios
+{
+    public interface IExcelExporter<T>
+        where T : class
+    {
+        void Export(IEnumerable<T> list, string path);
+    }
+}
